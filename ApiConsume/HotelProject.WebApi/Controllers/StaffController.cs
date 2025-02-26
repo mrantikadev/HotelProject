3236ead0df85eee6223043babbca63ff968ceea9
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-02-26 10:44:44 +0300

    Added simple controllers

---
+++ ApiConsume/HotelProject.WebApi/Controllers/StaffController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult StaffList()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStaff()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult AddStaff()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteStaff()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateStaff()
+        {
+            return Ok();
+        }
+    }
+}
