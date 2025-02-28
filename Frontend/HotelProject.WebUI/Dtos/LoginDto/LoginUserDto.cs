42209667ed0c423820973ce7ce1990d088bd6ccf
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-02-28 11:46:45 +0300

    Login

---
+++ Frontend/HotelProject.WebUI/Dtos/LoginDto/LoginUserDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.LoginDto
+{
+    public class LoginUserDto
+    {
+        [Required(ErrorMessage = "Kullanici adi giriniz")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Sifre giriniz")]
+        public string Password { get; set; }
+    }
+}
