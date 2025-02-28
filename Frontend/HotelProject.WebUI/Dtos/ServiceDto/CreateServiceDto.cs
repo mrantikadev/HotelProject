58fc0849ea62dfdc15a3b968c0850fb2e024c2d2
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-02-28 10:16:55 +0300

    Dtos for frontend

---
+++ Frontend/HotelProject.WebUI/Dtos/ServiceDto/CreateServiceDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.ServiceDto
+{
+    public class CreateServiceDto
+    {
+        [Required(ErrorMessage = "Hizmet ikon linki giriniz")]
+        public string ServiceIcon { get; set; }
+
+        [Required(ErrorMessage = "Hizmet basligi giriniz")]
+        [StringLength(100, ErrorMessage = "Hizmet basligi en fazla 100 karakter olabilir")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Hizmet aciklamasi giriniz")]
+        public string Description { get; set; }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/Dtos/ServiceDto/UpdateServiceDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.ServiceDto
+{
+    public class UpdateServiceDto
+    {
+        public int ServiceId { get; set; }
+
+        [Required(ErrorMessage = "Hizmet ikon linki giriniz")]
+        public string ServiceIcon { get; set; }
+
+        [Required(ErrorMessage = "Hizmet basligi giriniz")]
+        [StringLength(100, ErrorMessage = "Hizmet basligi en fazla 100 karakter olabilir")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Hizmet aciklamasi giriniz")]
+        [StringLength(500, ErrorMessage = "Hizmet aciklamasi en fazla 500 karakter olabilir")]
+        public string Description { get; set; }
+    }
+}
