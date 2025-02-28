2e2be1c5918d3d21a9ff69fc16ff2deb0a3ace78
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-02-28 09:49:48 +0300

    Dtos

---
+++ ApiConsume/HotelProject.DtoLayer/Dtos/RoomDto/RoomAddDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DtoLayer.Dtos.RoomDto
+{
+    public class RoomAddDto
+    {
+        [Required(ErrorMessage = "Lutfen oda numarasini giriniz")]
+        public string RoomNumber { get; set; }
+        public string RoomCoverImage { get; set; }
+
+        [Required(ErrorMessage = "Lutfen fiyat bilgisi giriniz")]
+        public int Price { get; set; }
+
+        [Required(ErrorMessage = "Lutfen oda basligi bilgisi giriniz")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Lutfen yatak sayisi bilgisi giriniz")]
+        public string BedCount { get; set; }
+
+        [Required(ErrorMessage = "Lutfen banyo sayisi bilgisi giriniz")]
+        public string BathCount { get; set; }
+        public string Wifi { get; set; }
+        public string Description { get; set; }
+    }
+}
