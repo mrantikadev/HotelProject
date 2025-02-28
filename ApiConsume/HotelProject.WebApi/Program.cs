0d0b3b7c0ef4621464ec297eed7908b07a62f3bb
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-02-28 09:56:20 +0300

    AutoMapper package

--- ApiConsume/HotelProject.WebApi/Program.cs
+++ ApiConsume/HotelProject.WebApi/Program.cs
@@ -24,6 +24,10 @@ builder.Services.AddScoped<ISubscribeService, SubscribeManager>();
 builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
 builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
 
+builder.Services.AddAutoMapper(typeof(Program));
+
+//builder.Services.AddScoped<>();
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("OtelApiCors", options =>
