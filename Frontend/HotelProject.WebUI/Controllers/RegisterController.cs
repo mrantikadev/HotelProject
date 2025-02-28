f37217de231caf21d15946ebed62d46ae912b93d
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-02-28 11:07:55 +0300

    CreateNewUserDto

---
+++ Frontend/HotelProject.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,29 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.RegisterDto;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CreateNewUserDto createNewUserDto)
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+    public class CreateNewUserDto
+    {
+        [Required(ErrorMessage = "Ad alani gereklidir")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Soyad alani gereklidir")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Kullanici adi alani gereklidir")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Mail alani gereklidir")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Sifre alani gereklidir")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Sifre tekrar alani gereklidir")]
+        [Compare("Password", ErrorMessage = "Sifreler uyusmuyor")]
+        public string ConfirmPassword { get; set; }
+    }
+}
