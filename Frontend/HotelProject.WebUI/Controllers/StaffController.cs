125886e133ae5619167837156bb3017ce4411e0e
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-02-28 08:55:07 +0300

    Created view for adding Staff

--- Frontend/HotelProject.WebUI/Controllers/StaffController.cs
+++ Frontend/HotelProject.WebUI/Controllers/StaffController.cs
@@ -1,6 +1,7 @@
 ﻿using HotelProject.WebUI.Models.Staff;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HotelProject.WebUI.Controllers
@@ -24,6 +25,28 @@ namespace HotelProject.WebUI.Controllers
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<StaffViewModel>>(jsonData);
                 return View(values);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddStaff()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddStaff(AddStaffViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7135/api/Staff", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
             }
 
             return View();
